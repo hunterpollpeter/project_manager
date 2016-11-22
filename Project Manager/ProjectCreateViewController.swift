0047d1e76c16c65625b4884da8206e19f0558550
Student <student@vsc161-13311.central.edu>
2016-11-22 13:57:32 -0600

    Added textfield delegate methods to check for done button and to prevent bad text from being input into date textfields

--- Project Manager/ProjectCreateViewController.swift
+++ Project Manager/ProjectCreateViewController.swift
@@ -11,6 +11,12 @@ import UIKit
 class ProjectCreateViewController: UIViewController, UITextFieldDelegate {
     
     var projectStore: ProjectStore!
+    var startDatePicker: UIDatePicker!
+    var deadlineDatePicker: UIDatePicker!
+    var startTimePicker: UIDatePicker!
+    var deadlineTimePicker: UIDatePicker!
+    
+    // MARK: Connections
     
     @IBOutlet var startDateTextField: UITextField!
     @IBOutlet var startTimeTextField: UITextField!
@@ -19,133 +25,11 @@ class ProjectCreateViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet var nameTextField: UITextField!
     @IBOutlet var detailsTextField: UITextField!
     
-    var startDatePicker: UIDatePicker!
-    var deadlineDatePicker: UIDatePicker!
-    var startTimePicker: UIDatePicker!
-    var deadlineTimePicker: UIDatePicker!
-    
-    override func viewDidLoad() {
-        navigationItem.title = "Create Project"
-        
-        let date = NSDate()
-        
-        let dateFormatter = NSDateFormatter()
-        
-        dateFormatter.dateStyle = .MediumStyle
-        dateFormatter.timeStyle = .NoStyle
-        
-        let dateString = dateFormatter.stringFromDate(date)
-        
-        startDateTextField.text = dateString
-        deadlineDateTextField.text = dateString
-        
-        dateFormatter.dateStyle = .NoStyle
-        dateFormatter.timeStyle = .ShortStyle
-        
-        let timeString = dateFormatter.stringFromDate(date)
-        
-        startTimeTextField.text = timeString
-        deadlineTimeTextField.text = timeString
-        
-        startDatePicker = UIDatePicker()
-        startDatePicker.datePickerMode = UIDatePickerMode.Date
-        startDatePicker.addTarget(self, action: #selector(datePickerValueChanged), forControlEvents: UIControlEvents.ValueChanged)
-        
-        deadlineDatePicker = UIDatePicker()
-        deadlineDatePicker.datePickerMode = UIDatePickerMode.Date
-        deadlineDatePicker.addTarget(self, action: #selector(datePickerValueChanged), forControlEvents: UIControlEvents.ValueChanged)
-        
-        startTimePicker = UIDatePicker()
-        startTimePicker.datePickerMode = UIDatePickerMode.Time
-        startTimePicker.addTarget(self, action: #selector(timePickerValueChanged), forControlEvents: UIControlEvents.ValueChanged)
-        
-        deadlineTimePicker = UIDatePicker()
-        deadlineTimePicker.datePickerMode = UIDatePickerMode.Time
-        deadlineTimePicker.addTarget(self, action: #selector(timePickerValueChanged), forControlEvents: UIControlEvents.ValueChanged)
-        
-        let doneButton = UIBarButtonItem(barButtonSystemItem: .Done, target: nil, action: nil)
-        doneButton.style = .Done
-        doneButton.action = #selector(responderDoneButtonClicked)
-        
-        let flexSpace = UIBarButtonItem(barButtonSystemItem: .FlexibleSpace, target: nil, action: nil)
-        
-        let toolBar = UIToolbar()
-        toolBar.barStyle = .Default
-        toolBar.setItems([flexSpace ,doneButton], animated: true)
-        toolBar.sizeToFit()
-        
-        startDateTextField.inputView = startDatePicker
-        startDateTextField.inputAccessoryView = toolBar
-        startDateTextField.delegate = self
-        
-        startTimeTextField.inputView = startTimePicker
-        startTimeTextField.inputAccessoryView = toolBar
-        
-        deadlineDateTextField.inputView = deadlineDatePicker
-        deadlineDateTextField.inputAccessoryView = toolBar
-        
-        deadlineTimeTextField.inputView = deadlineTimePicker
-        deadlineTimeTextField.inputAccessoryView = toolBar
-    }
-    
-    func datePickerValueChanged(sender: UIDatePicker) {
-        let dateFormatter = NSDateFormatter()
-        dateFormatter.dateStyle = .MediumStyle
-        dateFormatter.timeStyle = .NoStyle
-        let text = dateFormatter.stringFromDate(sender.date)
-        switch sender {
-        case startDatePicker:
-            startDateTextField.text = text
-        case deadlineDatePicker:
-            deadlineDateTextField.text = text
-        default:
-            return
-        }
-
-    }
-    
-    func timePickerValueChanged(sender: UIDatePicker) {
-        let dateFormatter = NSDateFormatter()
-        dateFormatter.dateStyle = .NoStyle
-        dateFormatter.timeStyle = .ShortStyle
-        let text = dateFormatter.stringFromDate(sender.date)
-        switch sender {
-        case startTimePicker:
-            startTimeTextField.text = text
-        case deadlineTimePicker:
-            deadlineTimeTextField.text = text
-        default:
-            return
-        }
-    }
-    
-    func responderDoneButtonClicked(sender: AnyObject) {
-        let dateFormatter = NSDateFormatter()
-        dateFormatter.dateStyle = .MediumStyle
-        dateFormatter.timeStyle = .NoStyle
-        if startDateTextField.isFirstResponder() {
-            startDateTextField.text = dateFormatter.stringFromDate(startDatePicker.date)
-        }
-        if deadlineDateTextField.isFirstResponder() {
-            deadlineDateTextField.text = dateFormatter.stringFromDate(deadlineDatePicker.date)
-        }
-        dateFormatter.dateStyle = .NoStyle
-        dateFormatter.timeStyle = .ShortStyle
-        if startTimeTextField.isFirstResponder() {
-            startTimeTextField.text = dateFormatter.stringFromDate(startTimePicker.date)
-        }
-        if deadlineTimeTextField.isFirstResponder() {
-            deadlineTimeTextField.text = dateFormatter.stringFromDate(deadlineTimePicker.date)
-        }
-
-        dismissKeybord()
-    }
-    
-    @IBAction func dismissKeybord() {
+    @IBAction func dismissKeyboard() {
         view.endEditing(false)
     }
     
-    @IBAction func Done(sender: AnyObject) {
+    @IBAction func Done() {
         let alertController = UIAlertController(title: "Failed to create project", message: nil, preferredStyle: .Alert)
         alertController.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
         let dateFormatter = NSDateFormatter()
@@ -201,5 +85,139 @@ class ProjectCreateViewController: UIViewController, UITextFieldDelegate {
         projectStore.createProject(name, details: details, start: start!, deadline: deadline!)
         self.parentViewController!.navigationController!.popViewControllerAnimated(true)
     }
+    
+    // MARK: TextField Delegates
+    
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        let dateFormatter = NSDateFormatter()
+        let currentString = textField.text!
+        switch textField {
+        case startDateTextField, deadlineDateTextField:
+            dateFormatter.dateFormat = "MMM d, yyyy"
+        case startTimeTextField, deadlineTimeTextField:
+            dateFormatter.dateFormat = "h:m a"
+        default:
+            return true
+        }
+        if let _ = dateFormatter.dateFromString(currentString + string) {
+            return true
+        }
+        else {
+            return false
+        }
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        dismissKeyboard()
+        return true
+    }
+    
+    // MARK: ViewController Lifecycle
+    
+    override func viewDidLoad() {
+        navigationItem.title = "Create Project"
+        
+        nameTextField.delegate = self
+        detailsTextField.delegate = self
+        
+        startDatePicker = UIDatePicker()
+        startDatePicker.datePickerMode = UIDatePickerMode.Date
+        startDatePicker.addTarget(self, action: #selector(datePickerValueChanged), forControlEvents: UIControlEvents.ValueChanged)
+        
+        deadlineDatePicker = UIDatePicker()
+        deadlineDatePicker.datePickerMode = UIDatePickerMode.Date
+        deadlineDatePicker.addTarget(self, action: #selector(datePickerValueChanged), forControlEvents: UIControlEvents.ValueChanged)
+        
+        startTimePicker = UIDatePicker()
+        startTimePicker.datePickerMode = UIDatePickerMode.Time
+        startTimePicker.minuteInterval = 15
+        startTimePicker.addTarget(self, action: #selector(timePickerValueChanged), forControlEvents: UIControlEvents.ValueChanged)
+        
+        deadlineTimePicker = UIDatePicker()
+        deadlineTimePicker.datePickerMode = UIDatePickerMode.Time
+        deadlineTimePicker.minuteInterval = 15
+        deadlineTimePicker.addTarget(self, action: #selector(timePickerValueChanged), forControlEvents: UIControlEvents.ValueChanged)
+        
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .Done, target: nil, action: nil)
+        doneButton.style = .Done
+        doneButton.action = #selector(responderDoneButtonClicked)
+        
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .FlexibleSpace, target: nil, action: nil)
+        
+        let toolBar = UIToolbar()
+        toolBar.barStyle = .Default
+        toolBar.setItems([flexSpace ,doneButton], animated: true)
+        toolBar.sizeToFit()
+        
+        startDateTextField.inputView = startDatePicker
+        startDateTextField.inputAccessoryView = toolBar
+        startDateTextField.delegate = self
+        
+        startTimeTextField.inputView = startTimePicker
+        startTimeTextField.inputAccessoryView = toolBar
+        startTimeTextField.delegate = self
+        
+        deadlineDateTextField.inputView = deadlineDatePicker
+        deadlineDateTextField.inputAccessoryView = toolBar
+        deadlineDateTextField.delegate = self
+        
+        deadlineTimeTextField.inputView = deadlineTimePicker
+        deadlineTimeTextField.inputAccessoryView = toolBar
+        deadlineTimeTextField.delegate = self
+    }
+    
+    // MARK: Functions
+    
+    func datePickerValueChanged(sender: UIDatePicker) {
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateStyle = .MediumStyle
+        dateFormatter.timeStyle = .NoStyle
+        let text = dateFormatter.stringFromDate(sender.date)
+        switch sender {
+        case startDatePicker:
+            startDateTextField.text = text
+        case deadlineDatePicker:
+            deadlineDateTextField.text = text
+        default:
+            return
+        }
 
+    }
+    
+    func timePickerValueChanged(sender: UIDatePicker) {
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateStyle = .NoStyle
+        dateFormatter.timeStyle = .ShortStyle
+        let text = dateFormatter.stringFromDate(sender.date)
+        switch sender {
+        case startTimePicker:
+            startTimeTextField.text = text
+        case deadlineTimePicker:
+            deadlineTimeTextField.text = text
+        default:
+            return
+        }
+    }
+    
+    func responderDoneButtonClicked(sender: AnyObject) {
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateStyle = .MediumStyle
+        dateFormatter.timeStyle = .NoStyle
+        if startDateTextField.isFirstResponder() {
+            startDateTextField.text = dateFormatter.stringFromDate(startDatePicker.date)
+        }
+        if deadlineDateTextField.isFirstResponder() {
+            deadlineDateTextField.text = dateFormatter.stringFromDate(deadlineDatePicker.date)
+        }
+        dateFormatter.dateStyle = .NoStyle
+        dateFormatter.timeStyle = .ShortStyle
+        if startTimeTextField.isFirstResponder() {
+            startTimeTextField.text = dateFormatter.stringFromDate(startTimePicker.date)
+        }
+        if deadlineTimeTextField.isFirstResponder() {
+            deadlineTimeTextField.text = dateFormatter.stringFromDate(deadlineTimePicker.date)
+        }
+
+        dismissKeyboard()
+    }
 }
