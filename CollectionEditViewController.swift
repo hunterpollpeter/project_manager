b5e57dc2a4b22564382cb1d4c42939e83d811158
Student <student@roe221-a-14060.central.edu>
2016-12-08 14:04:37 -0600

    Began implementing collection edit view controller

---
+++ CollectionEditViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionEditViewController.swift
+//  Project Manager
+//
+//  Created by Student on 12/8/16.
+//  Copyright © 2016 Hunter Pollpeter. All rights reserved.
+//
+
+import UIKit
+
+class CollectionEditViewController: UITableViewController {
+    
+}
